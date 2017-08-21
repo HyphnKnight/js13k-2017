bb9632efc3ff61360f361f8ca7933c4ae2f3b0b7
Brian Blakely <anewpage.media@gmail.com>
2017-08-21 18:59:11 -0400

    Added pointer hand emoji

--- src/graphics/emoji.js
+++ src/graphics/emoji.js
@@ -4,3 +4,4 @@ export const tree = `\uD83C\uDF32`;
 export const treeAlt = `\uD83C\uDF33`;
 export const cloud = `\u2601\uFE0F`;
 export const mountain = `\uD83C\uDFD4`;
+export const pointRight = `\uD83D\uDC49\uD83C\uDFFB`;
