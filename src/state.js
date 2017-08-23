e4d56218585e5aced57563b8386e7b0c8f0c32ea
HyphnKnight <rafe.lepre@skedge.me>
2017-08-23 00:03:25 -0400

    add State

---
+++ src/state.js
@@ -0,0 +1,18 @@
+const state = {
+  // Dialog is an array of dialog data
+  // dialog data is an array with the following values
+  // [text:string,author?:[emoji:string,name:string]];
+  // [
+  //   'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris mattis purus sed luctus dignissim. Phasellus hendrerit quam et urna tempor, eu porttitor dui feugiat. Praesent vestibulum est lectus, et vehicula velit laoreet non.',
+  //   [avenger, `Avenger${Math.floor(Math.random() * 10)}`],
+  // ]
+  dialog: [
+      [
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris mattis purus sed luctus dignissim. Phasellus hendrerit quam et urna tempor, eu porttitor dui feugiat. Praesent vestibulum est lectus, et vehicula velit laoreet non.',
+  ]
+  ],
+};
+
+
+
+export default state;
