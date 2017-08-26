374f57aaa3ecb0424c022cdc7fb7e5cb4c916e6b
Rafe Lepre <rafe.architect@me.com>
2017-08-26 16:36:01 +0000

    fix titlescreen variable use

--- src/titlescreen/index.js
+++ src/titlescreen/index.js
@@ -9,32 +9,34 @@ import { gem } from 'emoji';
 
 let animationStart = null;
 let animState = 0;
+let animTime = 0;
 
 const diamondAnimation = (animTime, height) => ([
   animTime / 24000 > 1
     ? -11
     : (Math.abs(((animTime - 1500) % 6000 / 600) - 5) - 2.5) * 10 - 11,
   height - Math.min(animTime / 24000, 1) * height * 1.125,
-])
+]);
 
-const fontStyle = { textBaseline: `middle`, style: 'white', font: title_text, };
+const fontStyle = { textBaseline: `middle`, style: `white`, font: title_text, };
 
 const title = {
   geometry: createRectangle([0, 0], 0, canvas.width, canvas.height),
   render: ({ ctx, fillText, fillPolygon }, el) => {
 
-    if (!animationStart) {
+    if(!animationStart) {
       animationStart = Date.now();
       playCanonD();
-    };
+    }
 
-    animState = Math.min((Date.now() - animationStart) / 24000, 1);
+    animTime = Date.now() - animationStart;
+    animState = Math.min(animTime / 24000, 1);
 
     fillText(
       fontStyle,
       [
         animTime / 24000 > 1 ? -11 : (Math.abs(((animTime - 1500) % 6000 / 600) - 5) - 2.5) * 10 - 11,
-        height - Math.min(animTime / 24000, 1) * height * 1.125,
+        canvas.height - Math.min(animTime / 24000, 1) * canvas.height * 1.125,
       ],
       gem
     );
@@ -45,24 +47,24 @@ const title = {
       `A L T E R`
     );
 
-    if (animState > 1.1) {
+    if(animTime > 24500) {
       ctx.font = base_text;
       const { width } = ctx.measureText(`new game`);
-      fillText({ style: 'white', font: base_text }, [-width / 2 - 2, 48], `new game`);
+      fillText({ style: `white`, font: base_text }, [-width / 2 - 2, 48], `new game`);
       (Date.now() % 600 > 400) && fillPolygon(`white`, [-42, 44], [-5, 3, 5, 0, -5, -3]);
     }
 
-    if (inputs.space && animState < 1) animationStart /= 2;
-    else if (inputs.space) console.log('Start Game');
+    if(inputs.space && animState < 1) animationStart /= 2;
+    else if(inputs.space) console.log(`Start Game`);
 
   },
   interact: {
     onMouseDown() {
-      if (animState < 1) animationStart /= 2;
-      else console.log('Start Game');
+      if(animState < 1) animationStart /= 2;
+      else console.log(`Start Game`);
     }
   }
-}
+};
 
 export default () => {
   animationStart = null;
