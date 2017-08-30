34637d5b59c778d37c279fa2515585401a956bbe
HyphnKnight <rafe.lepre@skedge.me>
2017-08-30 12:35:27 -0400

    fix build

--- rollup.config.dev.js
+++ rollup.config.dev.js
@@ -3,6 +3,7 @@ import resolve from 'rollup-plugin-node-resolve';
 import includePaths from 'rollup-plugin-includepaths';
 
 export default {
+  name: `Alter`,
   input: `src/index.js`,
   output: {
     file: `dev/script.js`,
