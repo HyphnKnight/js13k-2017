9b3776c752cd4a0c540f0efd9eb4ca8a4f16469f
HyphnKnight <rafe.lepre@skedge.me>
2017-08-31 15:17:29 -0400

    Rename to include enemies as well as allies

---
+++ src/characters.js
@@ -0,0 +1,33 @@
+import { child, avenger, protector, persecutor } from 'emoji';
+
+export const Child = {
+  name: `Child`,
+  emoji: child,
+  maxHealth: 40,
+  movement: 3,
+  ability: `forget`,
+};
+
+export const Avenger = {
+  name: `Avenger`,
+  emoji: avenger,
+  maxHealth: 80,
+  movement: 3,
+  ability: `avenge`,
+};
+
+export const Protector = {
+  name: `Protector`,
+  emoji: protector,
+  maxHealth: 100,
+  movement: 2,
+  ability: `protect`,
+};
+
+export const Persecutor = {
+  name: `Persecutor`,
+  emoji: persecutor,
+  maxHealth: 60,
+  movement: 5,
+  ability: `persecute`,
+};
