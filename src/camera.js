a365ec3aaa31de95cda3d2cfc44ce710bd426abb
HyphnKnight <rafe.lepre@skedge.me>
2017-08-31 15:17:02 -0400

    Export calcWorldPosition

--- src/camera.js
+++ src/camera.js
@@ -1,5 +1,6 @@
-import { perspective } from 'perspective';
+import { perspective, reversePerspective } from 'perspective';
 
 export const camera = [0, 80, 140];
 
 export const calcScreenPosition = perspective(camera);
+export const calcWorldPosition = reversePerspective(camera);
