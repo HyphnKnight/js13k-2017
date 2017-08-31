da4611857dc5dfe850a7bd153aa3c33364aa3278
Brian Blakely <anewpage.media@gmail.com>
2017-08-31 16:38:24 -0400

    Output dev bundle to /dev.

--- rollup.config.dev.js
+++ rollup.config.dev.js
@@ -1,6 +1,8 @@
 import serve from 'rollup-plugin-serve';
 import config from './rollup.config.prod.js';
 
+config.output.file = `dev/script.js`;
+
 config.plugins.push(serve({
   open: true,
   contentBase: `dev`,
