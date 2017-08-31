049cf76989b92a7f75a144ac7bedc49ccce52399
HyphnKnight <rafe.lepre@skedge.me>
2017-08-31 15:16:33 -0400

    Add reversePerspective

--- src/perspective.js
+++ src/perspective.js
@@ -3,7 +3,7 @@ import { viewWidth, viewHeight } from 'dom';
 export const perspective =
   // camera coords in 3d space
   (camera) =>
-    // 2d point
+    // 2d point in world space
     ([pX, pY]) => {
       const [cX, cY, cZ] = camera;
       return [
@@ -12,3 +12,14 @@ export const perspective =
         Math.sqrt(Math.pow(cY + pY, 2) + Math.pow((cX - pX), 2)),
       ];
     };
+
+export const reversePerspective =
+  // camera coords in 3d space
+  (camera) =>
+    // 2d point on screen
+    ([sX, sY]) => {
+      const [cX, cY, cZ] = camera;
+      const pY = cY / (-cZ / (sY - viewHeight) - 1);
+      const pX = -((-2 * cX * cY) + (viewWidth * pY) + (viewWidth * cY) + (-2 * sX * pY) + (-2 * sX * cY)) / (2 * cY);
+      return [pX, pY];
+    }
