7fa424ba322a69111a12a032dc4696828f2f4aab
HyphnKnight <rafe.lepre@skedge.me>
2017-08-31 15:20:15 -0400

    Update statusBar

--- src/battle/statusBar.js
+++ src/battle/statusBar.js
@@ -1,21 +1,22 @@
-import { createRectangle } from 'lib/geometry';
+import { createRectangle } from 'pura/geometry/tuple';
+import { fillText } from 'pura/canvas/tuple';
 import { canvas, ctx, viewHeight, viewWidth } from 'dom';
 import { heart, blackHeart } from 'emoji';
 
 const textSize = 12;
 const lineHeight = textSize * 1.2;
 
-let name = "Avenger";
-let maxHealth = Math.random() * 80 + 20
-let health = Math.random() * (maxHealth-10) + 10
+const name = `Avenger`;
+const maxHealth = Math.random() * 80 + 20;
+const health = Math.random() * (maxHealth - 10) + 10;
 
 export const statusBar = {
-  geometry: createRectangle([0,-viewHeight/2+lineHeight/2], 0, viewWidth, lineHeight),
-  render({fillText},{ geometry }) {
+  geometry: createRectangle([0, -viewHeight / 2 + lineHeight / 2], 0, viewWidth, lineHeight),
+  render({ geometry }) {
     let heartContainers = Math.ceil(maxHealth / 10) + 1;
     let filledContainers = Math.ceil(health / 10) + 1;
-    while(--heartContainers>=0) fillText({ font: `6px mono` },[viewWidth/2 - 8*heartContainers,2],blackHeart);
-    while(--filledContainers>=0) fillText({ font: `6px mono` },[viewWidth/2 - 8*filledContainers,2],heart);
-    fillText({style:'white',font:`10px mono`},[-viewWidth/2,2],name);
+    while(--heartContainers >= 0) fillText({ font: `6px mono` }, [(viewWidth / 2 - 11) - 8 * heartContainers, 2], blackHeart);
+    while(--filledContainers >= 0) fillText({ font: `6px mono` }, [(viewWidth / 2 - 11) - 8 * filledContainers, 2], heart);
+    fillText({ style: `white`, font: `10px mono` }, [-viewWidth / 2, 2], name);
   }
-}
+};
