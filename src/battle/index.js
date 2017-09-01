057fcfdd70341804d5e10bd4336c6036b65128f6
HyphnKnight <rafe.lepre@skedge.me>
2017-09-01 00:01:04 -0400

    Convert battle/index to a scene

--- src/battle/index.js
+++ src/battle/index.js
@@ -1,7 +1,20 @@
-import { statusBar } from 'statusBar';
-import { uiElements, clearUi } from 'ui';
+import loop from 'loop';
+import StatusBar from 'statusBar';
+import { map } from 'map';
+import { render as renderUI, uiElements, clearUi } from 'ui';
+import { clear } from 'pura/canvas/tuple';
 
-export default () => {
-  clearUi();
-  uiElements.push(statusBar);
-};
+let stopLoop;
+
+export default {
+  init() {
+    uiElements.push(StatusBar);
+    stopLoop = loop(dt => {
+      clear();
+      renderUI();
+    });
+  },
+  dismiss() {
+    stopLoop();
+  }
+}
