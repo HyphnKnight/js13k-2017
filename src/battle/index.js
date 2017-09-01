d3e7ad6496d7a7ebf02364d50296dd81d0052182
HyphnKnight <rafe.lepre@skedge.me>
2017-09-01 14:38:16 -0400

    Update battle map

--- src/battle/index.js
+++ src/battle/index.js
@@ -1,17 +1,33 @@
 import loop from 'loop';
 import StatusBar from 'statusBar';
-import { map } from 'map';
+import BattleMap from 'map';
+import state from 'state';
+import { subtractSet } from 'pura/vector/tuple';
+import { canvasOffsetLeft, canvasOffsetTop, scaleX, scaleY } from 'dom';
 import { render as renderUI, uiElements, clearUi } from 'ui';
+import { calcWorldPosition } from 'camera';
 import { clear } from 'pura/canvas/tuple';
 
 let stopLoop;
 
+const calcMousePosition =
+  ({ clientX, clientY, touches }) =>
+    touches
+      ? ([touches[0].clientX, touches[0].clientY])
+      : ([clientX, clientY]);
+
 export default {
   init() {
-    uiElements.push(StatusBar);
+    uiElements.push(BattleMap, StatusBar);
     stopLoop = loop(dt => {
       clear();
       renderUI();
+    });
+    window.addEventListener('click', (evt) => {
+      const position = subtractSet(calcMousePosition(evt), [canvasOffsetLeft, canvasOffsetTop]);
+      position[0] *= scaleX;
+      position[1] *= scaleY;
+      state.target = calcWorldPosition(position);
     });
   },
   dismiss() {
