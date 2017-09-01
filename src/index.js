0adde257bbd3cacde37f286b4a115758a79acdb7
HyphnKnight <rafe.lepre@skedge.me>
2017-08-31 23:08:28 -0400

    Remove extra canvas stuff

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import titleScreen from 'titlescreen';
 import Scene from 'scene';
-import { setCanvas, ctx } from 'pura/canvas/tuple';
 
-export const canvas = document.querySelector(`canvas`);
-setCanvas(canvas);
 
 Scene(titleScreen);
