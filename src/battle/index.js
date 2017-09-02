a2fda0c397b32f60c6bd8fccbd3e55ccaee381ed
HyphnKnight <rafe.lepre@skedge.me>
2017-09-02 18:58:56 -0400

    Update battle map with input handling

--- src/battle/index.js
+++ src/battle/index.js
@@ -2,35 +2,21 @@ import loop from 'loop';
 import StatusBar from 'statusBar';
 import BattleMap from 'map';
 import state from 'state';
+import { inputs } from 'controls';
 import { subtractSet } from 'pura/vector/tuple';
 import { canvasOffsetLeft, canvasOffsetTop, scaleX, scaleY } from 'dom';
 import { render as renderUI, uiElements, clearUi } from 'ui';
 import { calcWorldPosition } from 'camera';
 import { clear } from 'pura/canvas/tuple';
 
-let stopLoop;
-
-const calcMousePosition =
-  ({ clientX, clientY, touches }) =>
-    touches
-      ? ([touches[0].clientX, touches[0].clientY])
-      : ([clientX, clientY]);
-
 export default {
   init() {
     uiElements.push(BattleMap, StatusBar);
-    stopLoop = loop(dt => {
-      clear();
-      renderUI();
-    });
-    window.addEventListener('click', (evt) => {
-      const position = subtractSet(calcMousePosition(evt), [canvasOffsetLeft, canvasOffsetTop]);
-      position[0] *= scaleX;
-      position[1] *= scaleY;
-      state.target = calcWorldPosition(position);
-    });
+    state.logic = () => {
+      if (inputs.mouse === 1) {
+        state.target = calcWorldPosition(inputs.mousePosition);
+      }
+    };
   },
-  dismiss() {
-    stopLoop();
-  }
+  dismiss() { }
 }
