701b351033d38927b0b129f6133e06af925b83d0
HyphnKnight <rafe.lepre@skedge.me>
2017-09-02 18:47:57 -0400

    Update with new input controls

--- src/overworld/index.js
+++ src/overworld/index.js
@@ -4,23 +4,11 @@ import { clear } from 'pura/canvas/tuple';
 import { subtractSet } from 'pura/vector/tuple';
 import state from 'state';
 import { Dialog } from 'dialog';
+import { inputs } from 'controls';
 import { render as renderGraphics } from 'overworld/graphics';
 import logic, { avngSprite } from 'overworld/logic';
 import { calcWorldPosition } from 'camera';
 import { canvasOffsetLeft, canvasOffsetTop, scaleX, scaleY } from 'dom';
-
-let stopLoop;
-
-const calcMousePosition =
-  ({ clientX, clientY, touches }) =>
-    touches
-      ? ([touches[0].clientX, touches[0].clientY])
-      : ([clientX, clientY]);
-
-let moveTo;
-const evtMouseDown = (e)=> (moveTo = e);
-const evtMouseMove = (e)=> moveTo && (moveTo = e);
-const evtMouseUp = ()=> (moveTo = false);
 
 export default {
   init: () => {
@@ -30,21 +18,10 @@ export default {
       logic(dt);
       renderGraphics(dt);
       // Input
-      if (moveTo) {
-        const position = subtractSet(calcMousePosition(moveTo), [canvasOffsetLeft, canvasOffsetTop]);
-        position[0] *= scaleX;
-        position[1] *= scaleY;
-        state.target = calcWorldPosition(position);
+      if (inputs.click) {
+        state.target = calcWorldPosition(inputs.mousePosition);
       }
     };
-
-    window.addEventListener(`mousedown`, evtMouseDown);
-    window.addEventListener(`mousemove`, evtMouseMove);
-    window.addEventListener(`mouseup`, evtMouseUp);
   },
-  dismiss: () => {
-    window.removeEventListener(`mousedown`, evtMouseDown);
-    window.removeEventListener(`mousemove`, evtMouseMove);
-    window.removeEventListener(`mouseup`, evtMouseUp);
-  },
+  dismiss: () => { },
 };
