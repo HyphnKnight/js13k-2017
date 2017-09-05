ce8422c0e9ec4045981326f47d07e784df4a8534
HyphnKnight <rafe.lepre@skedge.me>
2017-09-05 13:11:29 -0400

    Improve camera handling

--- src/battle/index.js
+++ src/battle/index.js
@@ -10,6 +10,7 @@ TODO:
 import StatusBar from 'statusBar';
 import BattleMap, { grid, mapOffset, gridScale } from 'map';
 import state from 'state';
+import { camera } from 'camera';
 import { inputs } from 'controls';
 import { getFromVector2d, vector2dToHex, hexToVector2d, get } from 'pura/hex';
 import { add, subtractSet, scaleSet } from 'pura/vector/tuple';
@@ -19,9 +20,15 @@ import { calcWorldPosition } from 'camera';
 export default {
   init() {
     uiElements.push(BattleMap, StatusBar);
+    camera[0] = 0;
+    camera[1] = 60;
+    camera[2] = 30;
     state.logic = () => {
+      if (camera[0] !== 0) camera[0] += -camera[0] * 0.01;
+      if (camera[1] !== 140) camera[1] += (140 - camera[1]) * 0.01;
+      if (camera[2] !== 250) camera[2] += (250 - camera[2]) * 0.01;
       const { click, mousePosition } = inputs;
-      if(click === 1) {
+      if (click === 1) {
         state.target = getFromVector2d(grid)(scaleSet(subtractSet(calcWorldPosition(mousePosition), mapOffset), 1 / gridScale));
       }
     };
