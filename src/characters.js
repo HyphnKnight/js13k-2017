707c645127b0b6000adedd8c91b11bac8afe8dfa
HyphnKnight <rafe.lepre@skedge.me>
2017-09-05 12:36:05 -0400

    Turn characters into a dictionary

--- src/characters.js
+++ src/characters.js
@@ -1,33 +1,35 @@
 import { child, avenger, protector, persecutor } from 'emoji';
 
-export const Child = {
-  name: `Child`,
-  emoji: child,
-  maxHealth: 40,
-  movement: 3,
-  ability: `forget`,
-};
+export default {
+  child: {
+    name: `Child`,
+    emoji: child,
+    maxHealth: 40,
+    movement: 3,
+    ability: `forget`,
+  },
 
-export const Avenger = {
-  name: `Avenger`,
-  emoji: avenger,
-  maxHealth: 80,
-  movement: 3,
-  ability: `avenge`,
-};
+  avenger: {
+    name: `Avenger`,
+    emoji: avenger,
+    maxHealth: 80,
+    movement: 3,
+    ability: `avenge`,
+  },
 
-export const Protector = {
-  name: `Protector`,
-  emoji: protector,
-  maxHealth: 100,
-  movement: 2,
-  ability: `protect`,
-};
+  protector: {
+    name: `Protector`,
+    emoji: protector,
+    maxHealth: 100,
+    movement: 2,
+    ability: `protect`,
+  },
 
-export const Persecutor = {
-  name: `Persecutor`,
-  emoji: persecutor,
-  maxHealth: 60,
-  movement: 5,
-  ability: `persecute`,
+  persecutor: {
+    name: `Persecutor`,
+    emoji: persecutor,
+    maxHealth: 60,
+    movement: 5,
+    ability: `persecute`,
+  },
 };
