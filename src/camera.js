30a0872f331cfb9584a9a4e6e7c3e6fd1439044f
HyphnKnight <rafe.lepre@skedge.me>
2017-09-06 15:14:52 -0400

    Update camera to handle all perspective functions

--- src/camera.js
+++ src/camera.js
@@ -1,13 +1,16 @@
 import { viewWidth, viewHeight } from 'dom';
+import { subtract } from 'pura/vector/tuple';
 
-export const camera = [0, 80, 140];
+export let focalLength = 80;
+export const camera = [0, 0, 140];
 
 export const perspective =
   // 2d point in world space
-  ([pX, pY]) => {
-    const [cX, cY, cZ] = camera;
-    const relPos = (pY + cY);
-    const sX = pX + pY * (cX - pX) / (pY + cY) - cX + viewWidth / 2;
+  (point) => {
+    const [pX, pY] = subtract(point, camera);
+    const [_x, _y, cZ] = camera;
+    const relPos = (pY + focalLength);
+    const sX = pX + pY * (0 - pX) / (pY + focalLength) - 0 + viewWidth / 2;
     let sY;
     if (relPos < 0) {
       sY = viewHeight + cZ * pY / relPos;
@@ -16,7 +19,7 @@ export const perspective =
     } else {
       sY = viewHeight;
     }
-    return [sX, sY, Math.pow(cY + pY, 2) + Math.pow((cX - pX), 2)];
+    return [sX, sY, Math.pow(focalLength + pY, 2) + Math.pow((0 - pX), 2)];
   };
 
 export const perspective2d =
