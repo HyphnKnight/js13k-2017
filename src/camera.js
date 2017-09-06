927fd56111d15aad010f01019e30ebbe26a61edd
HyphnKnight <rafe.lepre@skedge.me>
2017-09-06 15:00:21 -0400

    Move perspective into the camera file

--- src/camera.js
+++ src/camera.js
@@ -1,9 +1,32 @@
-import { perspective, reversePerspective } from 'perspective';
+import { viewWidth, viewHeight } from 'dom';
 
 export const camera = [0, 80, 140];
 
-export const calcScreenPosition = perspective(camera);
-export const calcScreenPosition2d =
+export const perspective =
+  // 2d point in world space
+  ([pX, pY]) => {
+    const [cX, cY, cZ] = camera;
+    const relPos = (pY + cY);
+    const sX = pX + pY * (cX - pX) / (pY + cY) - cX + viewWidth / 2;
+    let sY;
+    if (relPos < 0) {
+      sY = viewHeight + cZ * pY / relPos;
+    } else if (relPos > 0) {
+      sY = viewHeight - cZ * pY / relPos;
+    } else {
+      sY = viewHeight;
+    }
+    return [sX, sY, Math.pow(cY + pY, 2) + Math.pow((cX - pX), 2)];
+  };
+
+export const perspective2d =
   (vector) =>
-    calcScreenPosition(vector).slice(0, 2);
-export const calcWorldPosition = reversePerspective(camera);
+    perspective(vector).slice(0, 2);
+
+export const calcWorldPosition =
+  ([sX, sY]) => {
+    const [cX, cY, cZ] = camera;
+    const pY = cY / (-cZ / (sY - viewHeight) - 1);
+    const pX = -((-2 * cX * cY) + (viewWidth * pY) + (viewWidth * cY) + (-2 * sX * pY) + (-2 * sX * cY)) / (2 * cY);
+    return [pX, pY];
+  };
