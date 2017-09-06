93e9d8d9b8625232fb5f32545af2fa0da5552949
HyphnKnight <rafe.lepre@skedge.me>
2017-09-06 16:49:26 -0400

    Camera changes

--- src/camera.js
+++ src/camera.js
@@ -1,25 +1,27 @@
 import { viewWidth, viewHeight } from 'dom';
 import { subtract } from 'pura/vector/tuple';
 
-export let focalLength = 80;
-export const camera = [0, 0, 140];
+export let focalLength = 500;
+export const camera = [0, 0, 240];
 
 export const perspective =
   // 2d point in world space
   (point) => {
     const [pX, pY] = subtract(point, camera);
-    const [_x, _y, cZ] = camera;
+    const cZ = camera[2];
     const relPos = (pY + focalLength);
-    const sX = pX + pY * (0 - pX) / (pY + focalLength) - 0 + viewWidth / 2;
+    let sX = pX + pY * -pX / (pY + focalLength) + viewWidth / 2;
     let sY;
     if (relPos < 0) {
       sY = viewHeight + cZ * pY / relPos;
+      sX = pX;
     } else if (relPos > 0) {
       sY = viewHeight - cZ * pY / relPos;
     } else {
       sY = viewHeight;
+      sX = pX;
     }
-    return [sX, sY, Math.pow(focalLength + pY, 2) + Math.pow((0 - pX), 2)];
+    return [sX, sY, Math.pow(focalLength + pY, 2) + Math.pow(-pX, 2)];
   };
 
 export const perspective2d =
@@ -28,8 +30,8 @@ export const perspective2d =
 
 export const calcWorldPosition =
   ([sX, sY]) => {
-    const [cX, cY, cZ] = camera;
-    const pY = cY / (-cZ / (sY - viewHeight) - 1);
-    const pX = -((-2 * cX * cY) + (viewWidth * pY) + (viewWidth * cY) + (-2 * sX * pY) + (-2 * sX * cY)) / (2 * cY);
-    return [pX, pY];
+    const cZ = camera[2];
+    const pY = focalLength / (-cZ / (sY - viewHeight) - 1);
+    const pX = -((viewWidth * pY) + (viewWidth * focalLength) + (-2 * sX * pY) + (-2 * sX * focalLength)) / (2 * focalLength);
+    return addSet([pX, pY], camera);
   };
