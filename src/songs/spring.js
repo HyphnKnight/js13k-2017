368bdf8e9e0d94ed5965c3f178b01301f84f6b9c
Brian Blakely <anewpage.media@gmail.com>
2017-09-08 21:27:18 -0400

    Spring added.

---
+++ src/songs/spring.js
@@ -0,0 +1,33 @@
+import * as note from 'audio/notes';
+
+const song = [
+  [``, 0.5],
+  [``, 0.5],
+  [note.E5, 0.5],
+  [note.G5, 0.5],
+  [note.G5, 0.5],
+  [note.G5, 0.25],
+  [note.F5, 0.25],
+  [note.E5, 1],
+  [note.B5, 0.25],
+  [note.B5, 0.25],
+  [note.A5, 0.5],
+  [note.G5, 0.5],
+  [note.G5, 0.5],
+  [note.G5, 0.25],
+  [note.F5, 0.25],
+  [note.E5, 1],
+  [note.B5, 0.25],
+  [note.B5, 0.25],
+  [note.A5, 0.5],
+  [note.G5, 0.25],
+  [note.A5, 0.25],
+  [note.B5, 0.5],
+  [note.A5, 0.5],
+  [note.G5, 0.5],
+  [note.F5, 0.5],
+  [note.D5, 0.5],
+  [note.B4, 0.5],
+];
+
+export default song;
