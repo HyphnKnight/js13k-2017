58f6536fa28f0b39672b6bfecd8563245534e481
HyphnKnight <rafe.lepre@skedge.me>
2017-09-09 14:56:44 -0400

    Update battle map with new strategies

--- src/battle/map.js
+++ src/battle/map.js
@@ -1,45 +1,22 @@
-import { createRectangle, createEqualLateralPolygon, getRectanglePoints } from 'pura/geometry/tuple';
-import { mapListSet, addSet, addList, addListSet, mapList, scaleSet } from 'pura/vector/tuple';
+import { createRectangle, createEqualLateralPolygon } from 'pura/geometry/tuple';
+import { addList, mapList, scaleSet } from 'pura/vector/tuple';
 import { ctx, fillRectangle, fillPolygon, strokePolygon, fillText } from 'pura/canvas/tuple';
-import { flatten, contains } from 'pura/array';
-import { generateGrid, hexToVector2d } from 'pura/hex';
+import { contains } from 'pura/array';
+import { hexToVector2d } from 'pura/hex';
 import { viewHeight, viewWidth } from 'dom';
 import { perspective2d } from 'camera';
-import { inputs, keyboardVector } from 'controls';
-import characters from 'characters';
+import { battleData, optionHexes, turnOrder } from 'battle/grid';
 import state from 'state';
 
+const gridColor = `white`;
+const selectColor = `#fcfc68`;
+const optionColor = `#78005c`;
 
-export const mapOffset = [0, 0];
+const baseHex = createEqualLateralPolygon([0, 0], 0, 6, 20);
 
-export const gridScale = 20;
-
-export const grid = generateGrid(5);
-
-const gridColor = `#C04848`;
-const selectColor = `#F07241`;
-const optionColor = `#C04848`;
-
-export const battleData = new Map(flatten(grid).map(hex => [hex, {
-  hex,
-  entity: null,
-  status: [],
-  position: null,
-  points: null,
-}]));
-
-const randomRow = grid[Math.floor(Math.random() * grid.length)];
-const randomHex = randomRow[Math.floor(Math.random() * randomRow.length)];
-battleData.get(randomHex).entity = { name: `protector`, health: 70 };
-
-const options = grid[Math.floor(Math.random() * grid.length)];
-let selectedOption = null;
-
-const baseHex = createEqualLateralPolygon([0, 0], 0, 6, gridScale);
-
-const calculateHexGeometry = hex => {
-  const data = battleData.get(hex);
-  const position = addSet(scaleSet(hexToVector2d(hex), gridScale), mapOffset);
+const calculateHexGeometry = data => {
+  const { hex } = data;
+  const position = scaleSet(hexToVector2d(hex), 20);
   const viewPosition = perspective2d(position);
   const points = mapList(addList(baseHex.points, position), perspective2d);
   data.position = viewPosition;
@@ -48,32 +25,33 @@ const calculateHexGeometry = hex => {
 };
 
 const drawFill =
-  ({ hex, points }) =>
-    state.target === hex ? Date.now() % 600 > 400 && fillPolygon(selectColor, [0, 0], points, 0) :
-      contains(options, hex) ? Date.now() % 600 > 400 && fillPolygon(optionColor, [0, 0], points, 0) :
-        null;
+  (options) =>
+    ({ hex, points }) => {
+      if(state.target === hex) Date.now() % 600 > 400 && fillPolygon(selectColor, [0, 0], points, 0);
+      if(contains(options, hex)) {
+        Date.now() % 600 > 400 && fillPolygon(optionColor, [0, 0], points, 0);
+      }
+    };
 
 const drawOutline =
   ({ points }) =>
     strokePolygon({ style: gridColor, thickness: 1 }, [0, 0], points, 0);
 
+// TODO: Draw different things based on status effects (hots/dots).
+const entityTextStyle = { horizontalAlign: true };
 const drawEntity =
-  ({ entity, position }) =>
-    entity
-      ? fillText({ horizontalAlign: true }, position, characters[entity.name].emoji)
-      : null;
+  ([data, , position,]) =>
+    fillText(entityTextStyle, perspective2d(position), data.emoji);
 
 const groundGradient = ctx.createLinearGradient(-100, -100, 200, 200);
-groundGradient.addColorStop(0, `#300030`);
-groundGradient.addColorStop(1, `#601848`);
+groundGradient.addColorStop(0, `#480078`);
+groundGradient.addColorStop(1, `#78005c`);
 
 const skyGradient = ctx.createLinearGradient(0, 0, 100, 100);
 skyGradient.addColorStop(0, `#F07241`);
 skyGradient.addColorStop(1, `#601848`);
 
-const keyControls = keyboardVector(3);
-
-export default {
+export default ({
   geometry: createRectangle([-viewWidth / 2, -viewHeight / 2], 0, viewWidth, viewHeight),
   render() {
     // Background
@@ -84,38 +62,9 @@ export default {
       viewHeight * 2,
       0,
     );
-
-    fillPolygon(
-      groundGradient,
-      [0, 0],
-      mapListSet(
-        addListSet(getRectanglePoints(10000, 1600), [0, 1600 / 2]),
-        pnt => perspective2d(pnt),
-      ),
-    );
-
-    [].concat(...grid).forEach(calculateHexGeometry);
-    battleData.forEach(drawFill);
+    battleData.forEach(calculateHexGeometry);
+    battleData.forEach(drawFill(optionHexes));
     battleData.forEach(drawOutline);
-    battleData.forEach(drawEntity);
-
-    addSet(mapOffset, keyControls());
-    if(state.target !== null) {
-      const targetIndex = options.indexOf(state.target);
-      if(targetIndex > -1) selectedOption = targetIndex;
-      else selectedOption = null;
-    }
-    if(selectedOption === null && (inputs.q === 1 || inputs.e === 1)) {
-      selectedOption = 0;
-    } else if(inputs.e === 1) {
-      selectedOption = ++selectedOption === options.length
-        ? 0
-        : selectedOption;
-    } else if(inputs.q === 1) {
-      selectedOption = --selectedOption < 0
-        ? options.length - 1
-        : selectedOption;
-    }
-    if(selectedOption !== null) state.target = options[selectedOption];
+    turnOrder.forEach(drawEntity);
   }
-};
+});
