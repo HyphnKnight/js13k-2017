23c5ff12b54bf91c7ca4aac9da419b46633946d6
Brian Blakely <anewpage.media@gmail.com>
2017-09-09 13:09:09 -0400

    Overworld music.

---
+++ src/songs/overworld.js
@@ -0,0 +1,38 @@
+import * as note from 'audio/notes';
+
+const song = [
+  [[note.E4, note.C4], 1],
+  [``, 1],
+  [[note.D4, note.G3], 1],
+  [``, 1],
+  [[note.C4, note.A3], 1],
+  [``, 1],
+  [[note.B3, note.E3], 1],
+  [``, 1],
+  [[note.A3, note.F3], 1],
+  [``, 1],
+  [[note.G3, note.C3], 1],
+  [``, 1],
+  [[note.A3, note.F3], 1],
+  [``, 1],
+  [[note.B3, note.G3], 1],
+  [``, 1],
+  [[note.E6, note.G5, note.C5], 1],
+  [``, 1],
+  [[note.D6, note.B5, note.G4], 1],
+  [``, 1],
+  [[note.C6, note.A4], 1],
+  [``, 1],
+  [[note.B5, note.G5, note.E4], 1],
+  [``, 1],
+  [[note.A5, note.C5, note.F4], 1],
+  [``, 1],
+  [[note.G5, note.E5, note.C4], 1],
+  [``, 1],
+  [[note.A5, note.F5, note.F4], 1],
+  [``, 1],
+  [[note.B5, note.D5, note.G4], 1],
+  [``, 1]
+];
+
+export default song;
