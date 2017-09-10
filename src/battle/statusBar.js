42c0e41f717cf16e433672b23de47856172a2592
HyphnKnight <rafe.lepre@skedge.me>
2017-09-09 23:13:32 -0400

    Fix StatusBar

--- src/battle/statusBar.js
+++ src/battle/statusBar.js
@@ -7,26 +7,25 @@ import { createRectangle } from 'pura/geometry/tuple';
 import { fillText } from 'pura/canvas/tuple';
 import { viewHeight, viewWidth } from 'dom';
 import { heart, heartBlack } from 'emoji';
-import { battleData } from 'battle/grid';
-import characters from 'characters';
+import { getCharacterAtHex } from 'battle/grid';
 import state from 'state';
 
 const textSize = 12;
 const lineHeight = textSize * 1.2;
 
-const createStatusBar = ({
+const StatusBar = ({
   geometry: createRectangle([0, -viewHeight / 2 + lineHeight / 2], 0, viewWidth, lineHeight),
   render() {
-    const hexData = battleData.get(state.target);
-    if(!hexData || !hexData.entity) return;
-    const { entity } = hexData;
-    const { maxHealth, name } = characters[entity.name];
+    const character = getCharacterAtHex(state.target);
+    if(!character) return;
+    const [entity, health] = character;
+    const { maxHealth, name } = entity;
     let heartContainers = Math.ceil(maxHealth / 10) + 1;
-    let filledContainers = Math.ceil(entity.health / 10) + 1;
+    let filledContainers = Math.ceil(health / 10) + 1;
     while(--heartContainers >= 0) fillText({ font: `6px mono` }, [(viewWidth / 2 - 11) - 8 * heartContainers, 2], heartBlack);
     while(--filledContainers >= 0) fillText({ font: `6px mono` }, [(viewWidth / 2 - 11) - 8 * filledContainers, 2], heart);
     fillText({ style: `white`, font: `10px mono` }, [-viewWidth / 2, 2], name);
   }
 });
 
-export default createStatusBar;
+export default StatusBar;
