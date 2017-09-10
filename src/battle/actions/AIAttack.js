ab7fe42b0570dddc4a3ef893aef8bccd8032cbe0
HyphnKnight <rafe.lepre@skedge.me>
2017-09-10 11:52:11 -0400

    Add AIAttack Ability

---
+++ src/battle/actions/AIAttack.js
@@ -0,0 +1,13 @@
+import mergeSort from 'pura/array/mergeSort';
+import PanCameraTo from 'battle/actions/PanCameraTo';
+
+export default function* AIAttack(character, nearbyGoodGuys) {
+  const [{ abilities: { aiAttack: { damage } } }] = character;
+  console.log(`1) Detect & Display possible attackers`);
+  const target = mergeSort(nearbyGoodGuys, ([, health]) => health)[0];
+  console.log(`2) Inflict damage on target`);
+  target[1] -= damage;
+  yield* PanCameraTo(target[2]);
+  console.log(`4) Play animation`);
+  yield;
+}
