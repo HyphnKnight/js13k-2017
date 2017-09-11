194c7f782c387fc3ad8ce01e9223c9803279a136
HyphnKnight <rafe.lepre@skedge.me>
2017-09-11 14:41:06 -0400

    Remove console.log

--- src/battle/actions/Swarm.js
+++ src/battle/actions/Swarm.js
@@ -14,11 +14,7 @@ import MoveCharacterOnPath from 'battle/actions/MoveCharacterOnPath';
 
 export default function* Swarm(character) {
   const [{ abilities: { swarm: { range } } }, , position] = character;
-  console.log(`1) Move Camera to enemy`);
   yield* PanCameraTo(position);
-  console.log(`2) Fetch all good units`);
-  console.log(`3) Sort by which one has the most enemies surrounding them`);
-  console.log(`4) Break tie by which is closest`);
   const badGuys = getNearbyEnemies(position, 10);
   const goodGuys = mergeSort(
     getNearbyAllies(position, 10),
@@ -28,13 +24,11 @@ export default function* Swarm(character) {
       0
     )
   );
-  console.log(`5) Fetch fastest path to target`);
   const [, , tPosition] = goodGuys[0];
   const path = getPathToTarget(
     getGridHexFromVector2d(position),
     getGridHexFromVector2d(tPosition)
   ).splice(0, range);
   path.pop();
-  console.log(`6) Move character while tracking with camera`);
   yield* MoveCharacterOnPath(position, path);
 }
