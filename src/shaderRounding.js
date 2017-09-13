de0cdd44a81bc55c48072b2ba0b1eba8bf739ad3
HyphnKnight <rafe.lepre@skedge.me>
2017-09-12 20:06:59 -0400

    Optimize

--- src/shaderRounding.js
+++ src/shaderRounding.js
@@ -1,21 +1,11 @@
 import { ctx } from 'pura/canvas/tuple';
 import { viewWidth, viewHeight } from 'dom';
 
-const offset = 75;
-
 const Shader = () => {
   const img = ctx.getImageData(0, 0, viewWidth, viewHeight);
   const data = img.data;
-  const dataLen = data.length;
-  let i = 0;
-  while(i < dataLen) {
-    data[i] = ((data[i] / offset) | 0) * offset;
-    data[i + 1] = ((data[i + 1] / offset) | 0) * offset;
-    data[i + 2] = ((data[i + 2] / offset) | 0) * offset;
-    i += 4;
-  }
-
-  // overwrite original image
+  let i = -1;
+  while(++i < data.length) data[i] = ((data[i] / 75) | 0) * 75;
   ctx.putImageData(img, 0, 0);
 };
 
