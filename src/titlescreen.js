dc28e4aa9023d35ee5da06d9c690cb683a475d63
Brian Blakely <anewpage.media@gmail.com>
2017-09-12 20:11:08 -0400

    Removed titlescreen animation sequence.

--- src/titlescreen.js
+++ src/titlescreen.js
@@ -1,55 +1,78 @@
 import { createRectangle } from 'pura/geometry/tuple';
+import { fillRectangle, fillPolygon, fillText } from 'pura/canvas/tuple';
 import { viewWidth, viewHeight } from 'dom';
+import { gem } from 'emoji';
 // import Song from 'audio';
 // import canond from 'songs/canond';
 import { inputs } from 'controls';
+import { title_text, base_text, white } from 'style';
 import state from 'state';
 import { uiElements } from 'ui';
 import Scene from 'scene';
 import overworld from 'overworld';
-import Movie from 'movie';
-import titleMovie, { endTitle } from 'movies/title';
-import intro from 'movies/intro';
+// import Movie from 'movie';
+// import titleMovie, { endTitle } from 'movies/title';
+// import intro from 'movies/intro';
 
-let skipped = false,
-    inIntro = false;
+// let skipped = false;
+// inIntro = false;
 
 const skipTitle = ()=> {
-  if(skipped) {
-    if(!inIntro) {
-      inIntro = true;
+  // if(skipped) {
+  // if(!inIntro) {
+  // inIntro = true;
 
-      // bgMusic.stop();
-      title.children = [Movie(0, 0, viewWidth, viewHeight, intro, ()=> {
-        Scene(overworld);
-      })];
-    }
+  // bgMusic.stop();
+  // title.children = [Movie(0, 0, viewWidth, viewHeight, intro, ()=> {
+  Scene(overworld);
+  // })];
+  // }
 
-    return;
-  }
+  // return;
+  // }
 
-  skipped = true;
+  // skipped = true;
 
-  title.children.splice(0, title.children.length);
-  endTitle();
-  title.children.push(Movie(
-    0, 0, viewWidth, viewHeight, titleMovie
-  ));
+  // title.children.splice(0, title.children.length);
+  // endTitle();
+  // title.children.push(Movie(
+  // 0, 0, viewWidth, viewHeight, titleMovie
+  // ));
+};
+
+const bigText = {
+  textBaseline: `middle`,
+  style: white,
+  font: title_text,
+  horizontalAlign: true
 };
 
 const title = {
   geometry: createRectangle([0, 0], 0, viewWidth, viewHeight),
 
-  children: [Movie(
-    0, 0, viewWidth, viewHeight, titleMovie, ()=> {
-      skipped = true;
-    }
-  )],
+  // children: [Movie(
+  //   0, 0, viewWidth, viewHeight, titleMovie, ()=> {
+  //     skipped = true;
+  //   }
+  // )],
 
   render() {
-    if((inputs.space === 1 || inputs.return === 1)) {
+    if(inputs.space === 1 || inputs.return === 1) {
       skipTitle();
     }
+
+    fillRectangle(
+      `#00f`,
+      [0, 0],
+      viewWidth,
+      viewHeight,
+      0,
+    );
+
+    fillText(bigText, [0,0], `A L T E R`);
+    fillText(bigText, [0,-24], gem);
+    fillText({ style: white, font: base_text }, [-28,51], `new game`);
+    (Date.now() % 600 > 400) && fillPolygon(`white`, [-42, 48], [-5, 3, 5, 0, -5, -3]);
   },
   interact: {
     onMouseDown: skipTitle
